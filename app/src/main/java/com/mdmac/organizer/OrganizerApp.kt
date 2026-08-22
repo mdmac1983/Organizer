18ae00d45a8788e34e3b3f1d5f9161a09433fad3
mdmac2683 <mdmac1983@proton.me>
2026-08-22 01:23:05 -0400

    Refactor OrganizerApp by removing theme settings
    Removed dark theme and dynamic color handling from OrganizerApp.

--- app/src/main/java/com/mdmac/organizer/OrganizerApp.kt
+++ app/src/main/java/com/mdmac/organizer/OrganizerApp.kt
@@ -1,25 +1,5 @@
 package com.mdmac.organizer
 
 import android.app.Application
-import androidx.appcompat.app.AppCompatDelegate
-import com.google.android.material.color.DynamicColors
 
-class OrganizerApp : Application() {
-
-    override fun onCreate() {
-        super.onCreate()
-
-        val prefs = getSharedPreferences("organizer_settings", MODE_PRIVATE)
-
-        val darkTheme = prefs.getBoolean("dark_theme", false)
-        AppCompatDelegate.setDefaultNightMode(
-            if (darkTheme) AppCompatDelegate.MODE_NIGHT_YES
-            else AppCompatDelegate.MODE_NIGHT_NO
-        )
-
-        val dynamicColor = prefs.getBoolean("dynamic_color", true)
-        if (dynamicColor) {
-            DynamicColors.applyToActivitiesIfAvailable(this)
-        }
-    }
-}
+class OrganizerApp : Application()
