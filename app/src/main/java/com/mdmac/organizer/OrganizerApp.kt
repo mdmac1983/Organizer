f33e4fdeeb94c76d83db1e64fbaa13a45b05a708
mdmac2683 <mdmac1983@proton.me>
2026-08-22 01:07:37 -0400

    Set default night mode and dynamic colors
    Initialize app theme settings based on user preferences.

--- app/src/main/java/com/mdmac/organizer/OrganizerApp.kt
+++ app/src/main/java/com/mdmac/organizer/OrganizerApp.kt
@@ -1,10 +1,25 @@
 package com.mdmac.organizer
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
+import com.google.android.material.color.DynamicColors
 
 class OrganizerApp : Application() {
+
     override fun onCreate() {
         super.onCreate()
-        // Global init hooks (database, etc.) will go here as we add tabs
+
+        val prefs = getSharedPreferences("organizer_settings", MODE_PRIVATE)
+
+        val darkTheme = prefs.getBoolean("dark_theme", false)
+        AppCompatDelegate.setDefaultNightMode(
+            if (darkTheme) AppCompatDelegate.MODE_NIGHT_YES
+            else AppCompatDelegate.MODE_NIGHT_NO
+        )
+
+        val dynamicColor = prefs.getBoolean("dynamic_color", true)
+        if (dynamicColor) {
+            DynamicColors.applyToActivitiesIfAvailable(this)
+        }
     }
 }
