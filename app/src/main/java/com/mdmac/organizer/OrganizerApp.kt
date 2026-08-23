c7be90ee78e7af33b67aabbd02c9b0ed66b3f657
mdmac2683 <mdmac1983@proton.me>
2026-08-23 18:55:16 -0400

    Simplify OrganizerApp class structure
    Removed theme preference initialization from OrganizerApp.
    
    Signed-off-by: mdmac2683 <mdmac1983@proton.me>

--- app/src/main/java/com/mdmac/organizer/OrganizerApp.kt
+++ app/src/main/java/com/mdmac/organizer/OrganizerApp.kt
@@ -1,11 +1,5 @@
 package com.mdmac.organizer
 
 import android.app.Application
-import com.mdmac.organizer.theme.ThemePreference
 
-class OrganizerApp : Application() {
-    override fun onCreate() {
-        super.onCreate()
-        ThemePreference(this).applyStoredMode()
-    }
-}
+class OrganizerApp : Application()
