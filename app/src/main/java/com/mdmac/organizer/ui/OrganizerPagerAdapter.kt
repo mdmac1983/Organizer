8b9ab42b9b5ada4e66d1b15797afb0c906a23dbf
mdmac2683 <mdmac1983@proton.me>
2026-08-23 02:12:05 -0400

    Remove AppsFragment and update item count
    Signed-off-by: mdmac2683 <mdmac1983@proton.me>

--- app/src/main/java/com/mdmac/organizer/ui/OrganizerPagerAdapter.kt
+++ app/src/main/java/com/mdmac/organizer/ui/OrganizerPagerAdapter.kt
@@ -2,7 +2,6 @@ package com.mdmac.organizer.ui
 
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.mdmac.organizer.ui.apps.AppsFragment
 import com.mdmac.organizer.ui.contacts.ContactsFragment
 import com.mdmac.organizer.ui.notes.NotesFragment
 import com.mdmac.organizer.ui.passwords.PasswordsFragment
@@ -10,14 +9,13 @@ import com.mdmac.organizer.ui.planner.PlannerFragment
 
 class OrganizerPagerAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity) {
 
-    override fun getItemCount(): Int = 5
+    override fun getItemCount(): Int = 4
 
     override fun createFragment(position: Int) = when (position) {
         0 -> PlannerFragment()
         1 -> ContactsFragment()
         2 -> PasswordsFragment()
         3 -> NotesFragment()
-        4 -> AppsFragment()
         else -> throw IllegalArgumentException("Invalid tab position: $position")
     }
 }
